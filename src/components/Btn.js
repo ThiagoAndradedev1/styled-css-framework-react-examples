e0444cba6bddd9de9c26bc29a392a493d5c69860
thiago.andrade.dev1@gmail.com <thiago.andrade.dev1@gmail.com>
2020-10-22 17:44:43 -0300

    test

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,5 +1,6 @@
 import React from 'react';
-// import {Button} from "styled-css-framework-react/core/Button";
+import Container from 'styled-css-framework-react/core/Container';
+
 
 
 
@@ -7,8 +8,9 @@ const Btn = () => {
     return (    
         <div style={{marginTop: "25px", textAlign: "center"}}>
         <h1 style={{ color: "#e53170", paddingBottom: "25px", paddingTop: "25px" }}>Buttons</h1> 
-        {/* <Container> */}
-        <Button pink>Hello World</Button>
+        <Container>
+            <h1>hello world</h1>
+        {/* <Button pink>Hello World</Button>
         <Button brown>Hello World</Button>
         <Button>Hello World</Button>
         <Button blue>Hello World</Button>
@@ -17,8 +19,8 @@ const Btn = () => {
         <Button gray>Hello World</Button>
         <Button purple>Hello World</Button>
         <Button black>Hello World</Button>
-        <Button orange>Hello World</Button>
-        {/* </Container> */}
+        <Button orange>Hello World</Button> */}
+        </Container>
         </div>
     )
 }
