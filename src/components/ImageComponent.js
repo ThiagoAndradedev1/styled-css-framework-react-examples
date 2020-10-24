feb3c1249707ef3fbbdca374caf2cd91c69396eb
thiago.andrade.dev1@gmail.com <thiago.andrade.dev1@gmail.com>
2020-10-24 11:56:36 -0300

    Correção de bugs no comp. ImageComponent

--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import Images from "styled-css-framework-react/core/Images";
+import Image from "styled-css-framework-react/core/Images";
 
 const ImageComponent = () => {
     return (    
         <div style={{marginTop: "25px", textAlign: "center"}}>
         <h1 style={{ color: "#e53170", paddingBottom: "25px", paddingTop: "25px" }}>Images</h1>
          <h1 style={{  color: "#fff", paddingBottom: "55px" }}>Circular and Rounded</h1>
-         <Images src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
-         <Images style={{ marginLeft: "15px" }} rounded src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
+         <Image src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
+         <Image style={{ marginLeft: "15px" }} rounded src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
          <h1 style={{  color: "#fff", paddingTop: "55px" }}>Sizes</h1>
          <h3 style={{  color: "#fff", paddingTop: "35px" }}>Small - Medium - Big - Large</h3>
          <div style={{ paddingTop: "35px", display: "inline-block" }}>
-         <Images small src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
-         <Images style={{ marginLeft: "5px" }} medium src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
-         <Images style={{ marginLeft: "5px" }} big src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
-         <Images style={{ marginLeft: "5px" }} large src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
+         <Image small src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
+         <Image style={{ marginLeft: "5px" }} medium src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
+         <Image style={{ marginLeft: "5px" }} big src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
+         <Image style={{ marginLeft: "5px" }} large src={'https://cdn.meutimao.com.br/_upload/noticia/2020/10/22/mantuan-marca-o-primeiro-gol-do-corinthians-na_j1941w.jpg'} />
          </div>
         </div>
     )
